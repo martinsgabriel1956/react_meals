016b9ced8786b89a2c2cfc404be6b70da3a6e437
Gabriel Martins <martinsgabriel1956@gmail.com>
2021-06-30 17:46:07 -0300

    feat: starting project

---
+++ src/styles/global.js
@@ -0,0 +1,18 @@
+import { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyle = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  html {
+    font-family: 'Noto Sans JP', sans-serif;
+  }
+
+  body {
+    margin: 0;
+    background-color: #3f3f3f;
+  }
+`;
