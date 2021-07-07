21a88478f5d73f5249bbc3201ad8520a06e15b48
Gabriel Martins <martinsgabriel1956@gmail.com>
2021-07-07 15:37:46 -0300

    feat: adding a checkout form

--- src/components/Cart/index.js
+++ src/components/Cart/index.js
@@ -1,34 +1,40 @@
-import { useContext } from 'react';
+import { useContext, useState } from 'react'
 
-import { CartItems, Total, Actions, Button, AltButton } from './styles';
+import { CartItems, Total, Actions, Button, AltButton } from './styles'
 
-import { Modal } from '../UI/Modal';
-import { CartItem } from '../Cart/CartItem';
+import { Modal } from '../UI/Modal'
+import { CartItem } from '../Cart/CartItem'
+import { Checkout } from '../Cart/Checkout'
 
-import { CartContext } from '../../contexts/CartContext';
+import { CartContext } from '../../contexts/CartContext'
 
 export function Cart(props) {
-  const { items, totalAmount, addItem, removeItem } = useContext(CartContext);
+  const [isCheckOut, setIsCheckOut] = useState(false)
+  const { items, totalAmount, addItem, removeItem } = useContext(CartContext)
 
-  const total = `$${totalAmount.toFixed(2)}`;
-  const hasItems = items.length > 0;
+  const total = `$${totalAmount.toFixed(2)}`
+  const hasItems = items.length > 0
 
   function handleCartItemRemove(id) {
-    removeItem(id);
+    removeItem(id)
   }
 
   function handleCartItemAdd(item) {
-    addItem(item);
+    addItem(item)
+  }
+
+  function handleOrder() {
+    setIsCheckOut(true)
   }
 
   return (
     <Modal onClose={props.onClose}>
       <CartItems>
-        {items.map(item => (
-          <CartItem 
-            key={item.id} 
-            name={item.name} 
-            amount={item.amount} 
+        {items.map((item) => (
+          <CartItem
+            key={item.id}
+            name={item.name}
+            amount={item.amount}
             price={item.price}
             onRemove={handleCartItemRemove.bind(null, item.id)}
             onAdd={handleCartItemAdd.bind(null, item)}
@@ -39,10 +45,14 @@ export function Cart(props) {
         <span>Total Amount</span>
         <span>{total}</span>
       </Total>
-      <Actions>
-        <AltButton onClick={props.onClose}>Close</AltButton>
-        {hasItems && <Button>Order</Button>}
-      </Actions>
+      {isCheckOut ? (
+        <Checkout onCancel={props.onClose} />
+      ) : (
+        <Actions>
+          <AltButton onClick={props.onClose}>Close</AltButton>
+          {hasItems && <Button onClick={handleOrder}>Order</Button>}
+        </Actions>
+      )}
     </Modal>
-  );
-};
+  )
+}
