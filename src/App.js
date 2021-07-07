eeedba5391637d7f329be5aa6a4eccd93cbc6229
Gabriel Martins <martinsgabriel1956@gmail.com>
2021-07-07 11:54:08 -0300

    feat: moving meals data to the backend

--- src/App.js
+++ src/App.js
@@ -1,30 +1,11 @@
-import { useState } from 'react';
-
+import { Home } from './pages/Home';
 import { GlobalStyle } from './styles/global';
 
-import { Header } from './components/Layout/Header';
-import { Meals } from './components/Meals/';
-import { Cart } from './components/Cart/';
-
 export function App() {
-  const [cartIsShown, setCartIsShown] = useState(false);
-
-  function handleShowCart() {
-    setCartIsShown(true)
-  }
-  
-  function handleHideCart() {
-    setCartIsShown(false)
-  }
-
   return (
     <>
       <GlobalStyle />
-      {cartIsShown && <Cart onClose={handleHideCart} />}
-      <Header onShowCart={handleShowCart} />
-      <main>
-        <Meals />
-      </main>
+      <Home />
     </>
   );
 }
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,5 +1,26 @@
+import { useState } from 'react';
+
+import { Header } from '../../components/Layout/Header';
+import { Meals } from '../../components/Meals/';
+import { Cart } from '../../components/Cart/';
+
 export function Home() {
+  const [cartIsShown, setCartIsShown] = useState(false);
+
+  function handleShowCart() {
+    setCartIsShown(true);
+  }
+
+  function handleHideCart() {
+    setCartIsShown(false);
+  }
   return (
-    <h1>Home</h1>
-  );
+    <>
+      {cartIsShown && <Cart onClose={handleHideCart} />}
+      <Header onShowCart={handleShowCart} />
+      <main>
+        <Meals />
+      </main>
+    </>
+  )
 }
