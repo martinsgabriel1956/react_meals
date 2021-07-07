fde8b9399876533ca0885e23326da9e7c1e6c4b2
Gabriel Martins <martinsgabriel1956@gmail.com>
2021-07-07 16:00:33 -0300

    feat: reading form values

--- src/components/Cart/Checkout/index.js
+++ src/components/Cart/Checkout/index.js
@@ -1,30 +1,45 @@
-import { Container, Control } from './styles';
+import { useRef } from 'react';
+import { Container, Control, Actions } from './styles'
 
 export function Checkout(props) {
+  const nameInputRef = useRef();
+  const streetInputRef = useRef();
+  const postalCodeInputRef = useRef();
+  const cityInputRef = useRef();
+  
   function handleConfirm(e) {
     e.preventDefault();
+
+    const name = nameInputRef.current.value;
+    const street = streetInputRef.current.value;
+    const postalCode = postalCodeInputRef.current.value;
+    const cityInput = cityInputRef.current.value;
   }
 
   return (
     <Container onSubmit={handleConfirm}>
       <Control>
-        <label htmlFor="name">Your Name</label>
-        <input type="text" id="name" />
+        <label htmlFor='name'>Your Name</label>
+        <input ref={nameInputRef} type='text' id='name' />
       </Control>
       <Control>
-        <label htmlFor="street">Street</label>
-        <input type="text" id="street" />
+        <label htmlFor='street'>Street</label>
+        <input ref={streetInputRef} type='text' id='street' />
       </Control>
       <Control>
-        <label htmlFor="postal">Postal</label>
-        <input type="text" id="postal" />
+        <label htmlFor='postal'>Postal</label>
+        <input ref={postalCodeInputRef} type='text' id='postal' />
       </Control>
       <Control>
-        <label htmlFor="city">City</label>
-        <input type="text" id="city" />
+        <label htmlFor='city'>City</label>
+        <input ref={cityInputRef} type='text' id='city' />
       </Control>
-      <button type="button" onClick={props.onCancel}>Cancel</button>
-      <button>Confirm</button>
+      <Actions>
+        <button type='button' onClick={props.onCancel}>
+          Cancel
+        </button>
+        <button className="submit">Confirm</button>
+      </Actions>
     </Container>
-  );
-};
+  )
+}
--- src/components/Cart/Checkout/styles.js
+++ src/components/Cart/Checkout/styles.js
@@ -2,8 +2,8 @@ import styled from 'styled-components'
 
 export const Container = styled.form`
   margin: 1rem 0;
-  height: 17rem;
-  overflow: auto;
+  height: 19rem;
+  overflow: scroll;
 `
 
 export const Control = styled.div`
@@ -21,5 +21,48 @@ export const Control = styled.div`
     border-radius: 4px;
     width: 20rem;
     max-width: 100%;
+  }
+`
+
+export const Actions = styled.div`
+  display: flex;
+  justify-content: flex-end;
+  gap: 1rem;
+
+  button {
+    font: inherit;
+    color: #5a1a01;
+    cursor: pointer;
+    background-color: transparent;
+    border: none;
+    border-radius: 25px;
+    padding: 0.5rem 2rem;
+
+    &::hover,
+    &:active {
+      background-color: #ffe6dc;
+    }
+  }
+
+  .submit {
+    border: 1px solid #5a1a01;
+    background-color: #5a1a01;
+    color: white;
+
+    &:hover,
+    &:active {
+      background-color: #7a2706;
+    }
+  }
+
+  .invalid {
+    label {
+      color: #ca3e51;
+    }
+
+    input {
+      border-color: #aa0b20;
+      background-color: #ffeff1;
+    }
   }
 `
